1c0e781f684d83f0b5ab931f2779253ef4befe7e
chandoorest-hub <chandoorest@gmail.com>
2026-09-07 17:09:38 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBnFS3Z3TiEZajNJKSNu6Nl8W7qTOcggBs",
+  authDomain: "chandoos-4c157.firebaseapp.com",
+  projectId: "chandoos-4c157",
+  storageBucket: "chandoos-4c157.firebasestorage.app",
+  messagingSenderId: "525807029372",
+  appId: "1:525807029372:web:f875fe6445ff72d8f50df9",
+  measurementId: "G-1JK7ZY9FCN"
+};
+
+const FIREBASE_DOC_PATH = { collection: "chandoos", docId: "menu-state" };
